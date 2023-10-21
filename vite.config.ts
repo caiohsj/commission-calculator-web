a255acdb851734227d60b5318f9daf4455f3c88d
Caio <caiocoxim10@gmail.com>
2023-10-21 11:01:58 -0400

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), VitePWA({ registerType: 'autoUpdate' })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
